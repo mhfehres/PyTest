67fd37bbbf97f9cdd49732381cb18e1d926c1f0f
megan.fehres <v86x213@esof423.cs.montana.edu>
2022-02-15 17:32:16 -0700

    initial checkin

--- example.py
+++ example.py
@@ -1,1 +1,15 @@
 # testing goes below
+
+def add(a, b):
+	return a + b
+
+def test_add():
+	assert add(2, 3) == 5
+	assert add('space', 'ship') == 'spaceship'
+
+def subtract(a, b):
+	return a + B # <--- fix this in step 8
+
+# uncomment the following test in step 5
+#def test_subtract():
+#	assert subtract(2, 3) == -1
